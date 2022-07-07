b6f02f21560f0aaef4616c2783c8c6cc58808fe9
Ирина Смирнова <ismirnova83@yandex.ru>
2022-07-08 00:33:19 +0300

    After the first review

--- homework.py
+++ homework.py
@@ -17,38 +17,30 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 600
+RETRY_TIME = 1
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
-    filename='main.log',
-)
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler(stream=sys.stdout)
-logger.addHandler(handler)
-
-
 def send_message(bot, message):
     """Отправляет сообщение в чат Telegram."""
+    if message == send_message.previous_message:
+        return
     try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
         logger.info('Сообщение успешно отправлено')
-        return bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        send_message.previous_message = message
     except Exception as error:
-        logger.error(f'Не удалось отправить сообщение {error}', exc_info=True)
-        raise exceptions.SendMessageException(error)
+        message = f'Не удалось отправить сообщение {error}'
+        raise exceptions.SendMessageException(message)
+send_message.previous_message = ''     
 
 
 def get_api_answer(current_timestamp):
@@ -56,16 +48,23 @@ def get_api_answer(current_timestamp):
     params = {'from_date': current_timestamp}
     try:
         answer = requests.get(ENDPOINT, headers=HEADERS, params=params)
+
+    except Exception as error:
+        error_message = f'Не удалось получить доступ к API: {error}'
+        raise exceptions.GetAPIException(error_message)
+
+    else:
         if answer.status_code != HTTPStatus.OK:
             status = answer.raise_for_status()
             error_message = f'Неверный статус ответа: {status}'
-            logger.error(error_message)
             raise exceptions.GetAPIException(error_message)
-        return answer.json()
-    except Exception:
-        error_message = 'Не удалось получить доступ к API'
-        logger.error(error_message)
-        raise exceptions.GetAPIException(error_message)
+
+        try:    
+            return answer.json()
+
+        except Exception as error:
+            error_message = 'Ошибка сериализации в json: {error}'
+            raise exceptions.JsonException(error_message)    
 
 
 def check_response(response):
@@ -74,12 +73,10 @@ def check_response(response):
         homework_list = response['homeworks']
     except KeyError as error:
         error_message = f'В словаре нет ключа homeworks {error}'
-        logger.error(error_message)
         raise KeyError(error_message)
 
     if not isinstance(homework_list, list):
         error_message = 'Домашние работы в ответе API выводятся не списком'
-        logger.error(error_message)
         raise exceptions.APIResponseException(error_message)
     return homework_list
 
@@ -91,12 +88,10 @@ def parse_status(homework):
         homework_status = homework['status']
     except KeyError as error:
         error_message = f'В словаре нет ключа: {error}'
-        logger.error(error_message)
         raise KeyError(error_message)
-    verdict = HOMEWORK_STATUSES.get(homework_status)
+    verdict = HOMEWORK_VERDICTS.get(homework_status)
     if verdict is None:
         error_message = 'Нет сообщения о статусе проверки домашней работы'
-        logger.error(error_message)
         raise exceptions.StatusException(error_message)
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
@@ -137,8 +132,21 @@ def main():
             logger.exception(f'Возникла ошибка: {error}')
             send_message(bot, message)
 
-        time.sleep(RETRY_TIME)
+        finally:
+            time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s, %(levelname)s, '
+           '%(name)s, %(funcName)s, '
+           '%(lineno)d, %(message)s',
+    filename='main.log',
+    )
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(stream=sys.stdout)
+    logger.addHandler(handler)
     main()
